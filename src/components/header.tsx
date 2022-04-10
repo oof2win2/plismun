afc91a5939be09ebaebe209ef3e8ee85c8b40b4c
oof2win2 <honza.koco44@gmail.com>
2022-04-10 22:48:27 +0200

    feat(site): header to chakra

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { Heading, Center } from "@chakra-ui/react"
 
 type HeaderProps =
   | {
@@ -10,65 +11,37 @@ type HeaderProps =
       mainPage?: undefined
     }
 
-// TODO: better looks on mobile
-
 export default ({ title, mainPage }: HeaderProps) => {
   return (
-    <section style={{ paddingBottom: "12vh", zIndex: -1 }}>
+    <section style={{ zIndex: -1, paddingBottom: mainPage ? "14vh" : "24vh" }}>
       <div
-        className="container"
+        className="bgWrap"
         style={{
-          width: "0px",
-          height: "40vh",
-          paddingBottom: mainPage ? "70vh" : "40vh",
           zIndex: -1,
+          height: mainPage ? "70vh" : "40vh",
         }}
       >
-        <div
-          className="bgWrap"
-          style={{
-            height: mainPage ? "70vh" : "40vh",
-            zIndex: -1,
-          }}
-        >
-          <Image
-            src={
-              mainPage
-                ? "/images/school_img2.jpg"
-                : "/images/school_imgheader.jpg"
-            }
-            layout="fill"
-            objectFit="cover"
-            className="backgroundImage"
-          />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignContent: "center",
-            alignItems: "center",
-            paddingTop: "16vh",
-            verticalAlign: "sub",
-            position: "absolute",
-            left: 0,
-            width: "100vw",
-            justifyContent: "center",
-            maxWidth: "100vw",
-            zIndex: -1,
-          }}
-          className="page animate"
-        >
-          {mainPage ? (
-            <>
-              <Image src="/images/logolarge.png" height={350} width={350} />
-              <h1 style={{ color: "white" }}>JANUARY 26TH - 29RD, 2023</h1>
-            </>
-          ) : (
-            <h1 style={{ color: "white" }}>{title}</h1>
-          )}
-        </div>
+        <Image
+          src={
+            mainPage
+              ? "/images/school_img2.jpg"
+              : "/images/school_imgheader.jpg"
+          }
+          layout="fill"
+          objectFit="cover"
+          className="backgroundImage"
+        />
       </div>
+      <Center paddingTop="16vh" flexDir="column">
+        {mainPage ? (
+          <>
+            <Image src="/images/logolarge.png" height={350} width={350} />
+            <Heading color="white">JANUARY 26TH - 29RD, 2023</Heading>
+          </>
+        ) : (
+          <Heading color="white">{title}</Heading>
+        )}
+      </Center>
     </section>
   )
 }
